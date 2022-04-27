4623801481ef12c1ecd93673318de1543601587f
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-04-26 20:41:46 -0400

    Tweaked app.vue to have the barebones of the application

--- app.vue
+++ app.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Name Generator</h1>
+
+    <p>Please select your options and click the "FIND NAMES" button bellow</p>
+
+    <div class="options-container">
+      <div class="gender-container">
+        <h4>1) Choose a gender:</h4>
+        <div class="options-buttons">
+          <button>Boy</button>
+          <button>Unisex</button>
+          <button>Girl</button>
+        </div>
+      </div>
+      <div class="gender-container">
+        <h4>2) Choose name's popularity:</h4>
+        <div class="options-buttons">
+          <button>Trendy</button>
+          <button>Unique</button>
+        </div>
+      </div>
+      <div class="gender-container">
+        <h4>3) Choose the name's length:</h4>
+        <div class="options-buttons">
+          <button>Long</button>
+          <button>All</button>
+          <button>Short</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
