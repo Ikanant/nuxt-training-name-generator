d0a7d909ddb98b8e5dac7f03a38115f9ca0e49c1
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-04-28 08:40:27 -0400

    Finish up setting up the Option component. It is now being re-used across the app

--- app.vue
+++ app.vue
@@ -99,35 +99,6 @@ h1 {
   position: relative;
 }
 
-.option-container {
-  margin-bottom: 2rem;
-}
-
-.option {
-  background-color: white;
-  outline: 0.15rem solid teal;
-  border: none;
-  padding: 0.75rem;
-  width: 12rem;
-  font-size: 1rem;
-  color: blue;
-  cursor: pointer;
-  font-weight: 200;
-}
-
-.option-left {
-  border-radius: 1rem 0 0 1rem;
-}
-
-.option-right {
-  border-radius: 0 1rem 1rem 0;
-}
-
-.option-active {
-  background-color: teal;
-  color: white;
-}
-
 .primary {
   background-color: teal;
   color: white;
@@ -138,6 +109,7 @@ h1 {
   margin-top: 1rem;
   cursor: pointer;
 }
+
 .card-container {
   display: flex;
   margin-top: 3rem;
--- components/Option.vue
+++ components/Option.vue
@@ -1,46 +1,81 @@
 <template>
   <div class="option-container">
-    <h4>{{ option.title }} </h4>
-    <!-- <div class="options-buttons">
-      <button
-        class="option option-left"
-        :class="options.gender === Gender.BOY && 'option-active'"
-        @click="options.gender = Gender.BOY"
-      >
-        Boy
-      </button>
+    <h4>{{ option.title }}</h4>
+    <div class="options-buttons">
       <button
         class="option"
-        :class="options.gender === Gender.UNISEX && 'option-active'"
-        @click="options.gender = Gender.UNISEX"
+        v-for="(button, index) in option.buttons"
+        :key="button"
+        :class="computeButtonClasses(button, index)"
+        @click="options[option.category] = button"
       >
-        Unisex
+        {{ button }}
       </button>
-      <button
-        class="option option-right"
-        :class="options.gender === Gender.GIRL && 'option-active'"
-        @click="options.gender = Gender.GIRL"
-      >
-        Girl
-      </button>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Gender, Popularity, Length } from "@/data"
-    interface OptionProps {
-        option: {
-            title:string;
-            category: string;
-            buttons: Gender[] | Popularity[] | Length[];
-        },
-        options: {
-            gender: Gender;
-            popularity: Popularity;
-            length: Length;
-        }
+import { Gender, Popularity, Length } from "@/data";
+interface OptionProps {
+  option: {
+    title: string;
+    category: string;
+    buttons: Gender[] | Popularity[] | Length[];
+  };
+  options: {
+    gender: Gender;
+    popularity: Popularity;
+    length: Length;
+  };
+}
+
+const props = defineProps<OptionProps>();
+
+const computeButtonClasses = (button, index) => {
+    const classNames:string[] = [];
+    if (props.options[props.option.category] === button) {
+        classNames.push("option-active");
     }
 
-    const props = defineProps<OptionProps>();
+    if (index == 0) {
+        classNames.push("option-left");
+    }
+    else if (index === props.option.buttons.length-1) {
+        classNames.push("option-right");
+    }
+
+    return classNames.join(" ");
+}
 </script>
+
+<style scoped>
+.option-container {
+  margin-bottom: 2rem;
+}
+
+.option {
+  background-color: white;
+  outline: 0.15rem solid teal;
+  border: none;
+  padding: 0.75rem;
+  width: 12rem;
+  font-size: 1rem;
+  color: blue;
+  cursor: pointer;
+  font-weight: 200;
+}
+
+.option-left {
+  border-radius: 1rem 0 0 1rem;
+}
+
+.option-right {
+  border-radius: 0 1rem 1rem 0;
+}
+
+.option-active {
+  background-color: teal;
+  color: white;
+}
+</style>
